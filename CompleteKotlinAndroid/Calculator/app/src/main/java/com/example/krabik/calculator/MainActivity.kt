c18d1572225cefa3db76b053f879e6c08d6ec2c3
KrabeOtake <cheshirekrab@gmail.com>
2017-09-07 23:43:21 +0300

    Unit 4 - Part 1 Pockemon app

--- CompleteKotlinAndroid/Calculator/app/src/main/java/com/example/krabik/calculator/MainActivity.kt
+++ CompleteKotlinAndroid/Calculator/app/src/main/java/com/example/krabik/calculator/MainActivity.kt
@@ -1,12 +1,98 @@
 package com.example.krabik.calculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buNumberEvent(view:View){
+        if(isNewOp == true){
+            etShownData.setText("")
+        }
+        isNewOp = false
+        val buSelected = view as Button
+        var buClickValue:String = etShownData.text.toString()
+        when(buSelected.id){
+            bu0.id -> buClickValue += "0"
+            bu1.id -> buClickValue += "1"
+            bu2.id -> buClickValue += "2"
+            bu3.id -> buClickValue += "3"
+            bu4.id -> buClickValue += "4"
+            bu5.id -> buClickValue += "5"
+            bu6.id -> buClickValue += "6"
+            bu7.id -> buClickValue += "7"
+            bu8.id -> buClickValue += "8"
+            bu9.id -> buClickValue += "9"
+            buDot.id -> buClickValue += "."
+            buPlusMinus.id -> buClickValue = "-" + buClickValue
+        }
+        etShownData.setText(buClickValue)
+
+    }
+
+    var op = ""
+    var oldNumber = ""
+    var isNewOp = true
+
+    fun buOpEvent(view:View){
+
+        val buSelected = view as Button
+        var buClickValue:String = etShownData.text.toString()
+        when(buSelected.id){
+            buPlus.id -> {
+                op = "+"
+            }
+            buMinus.id -> {
+                op = "-"
+            }
+            buMultiply.id -> {
+                op = "*"
+            }
+            buDivide.id -> {
+                op = "/"
+            }
+        }
+        oldNumber = etShownData.text.toString()
+        isNewOp = true
+
+    }
+
+    fun buEqualEvent(view:View){
+        var newNumber = etShownData.text.toString()
+        var finalNumber:Double? = null
+        when(op){
+            "+" -> {
+                finalNumber = oldNumber.toDouble() + newNumber.toDouble()
+            }
+            "-" -> {
+                finalNumber = oldNumber.toDouble() - newNumber.toDouble()
+            }
+            "*" -> {
+                finalNumber = oldNumber.toDouble() * newNumber.toDouble()
+            }
+            "/" -> {
+                finalNumber = oldNumber.toDouble() / newNumber.toDouble()
+            }
+        }
+        etShownData.setText(finalNumber.toString())
+        isNewOp = true
+    }
+
+    fun buPercent(view:View){
+        var number:Double = etShownData.text.toString().toDouble()/100
+        etShownData.setText(number.toString())
+    }
+
+    fun buClean(view:View){
+        etShownData.setText("")
+        isNewOp = true
     }
 }
---
+++ CompleteKotlinAndroid/PockemonAndroid/app/src/main/java/com/example/krabik/pockemonandroid/MapsActivity.kt
@@ -0,0 +1,145 @@
+package com.example.krabik.pockemonandroid
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        checkPermission()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
+    var location:Location? = null
+    var ACCESSLOCATION = 123
+
+    fun checkPermission(){
+        if(Build.VERSION.SDK_INT >= 23){
+            if(ActivityCompat.checkSelfPermission(this, android
+                    .Manifest
+                    .permission
+                    .ACCESS_FINE_LOCATION)!=PackageManager
+                    .PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), ACCESSLOCATION)
+                return
+            }
+        }
+        GetUserLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun GetUserLocation(){
+        Toast.makeText(this, "User location access on", Toast.LENGTH_SHORT).show()
+
+        var myLocation = GetMyLocation()
+
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 3f, myLocation)
+
+        var myThread = myThread()
+        myThread.start()
+    }
+
+     override fun onRequestPermissionsResult(requestCode: Int, permission: Array<out String>, grantResult: IntArray){
+
+        when(requestCode){
+            ACCESSLOCATION -> {
+                if(grantResult[0] == PackageManager.PERMISSION_GRANTED){
+                    GetUserLocation()
+                }else{
+                    Toast.makeText(this, "We cannot access your location", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permission, grantResult)
+    }
+
+    inner class GetMyLocation:LocationListener{
+
+        constructor(){
+            location = Location("Start")
+            location!!.longitude = 0.0
+            location!!.latitude = 0.0
+        }
+        override fun onLocationChanged(p0: Location?) {
+            location = p0
+        }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+    inner class myThread:Thread{
+        constructor():super(){
+
+        }
+
+        override fun run() {
+            while(true){
+                try{
+                    runOnUiThread {
+                        var sydney = LatLng(location!!.latitude, location!!.longitude)
+                        mMap.addMarker(MarkerOptions()
+                                .position(sydney)
+                                .title("Me")
+                                .snippet("Here is my location")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14f))
+                    }
+                    Thread.sleep(1000)
+                }catch (ex:Exception){
+
+                }
+            }
+        }
+    }
+
+
+}
